ed62c50d2529ec12e90011e00049cdcf5650075e
samuelscolari-blip <scolarisamuel@gmail.com>
2026-07-31 01:25:25 -0300

    Carregar padrão visual v52

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./ibs-cbs.css";
+import "./v52.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
