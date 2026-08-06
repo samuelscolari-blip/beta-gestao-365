c80aeb1bd28b2592ead02dc4a88f3a62278d87f1
samuelscolari-blip <scolarisamuel@gmail.com>
2026-08-06 11:55:40 -0300

    V131: restringir senha master somente ao ponto

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,21 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import AccessGate from "./components/AccessGate";
 import SecureBetaAppV131 from "./components/SecureBetaAppV131";
+import { masterPointSessionFromHeaders } from "./lib/master-point-access";
 import {
   authenticatedEmailFromHeaders,
   SOLE_ADMIN_EMAIL,
 } from "./lib/server-access";
-import {
-  businessStaffSessionFromHeaders,
-  hasStaffSessionCookie,
-} from "./lib/staff-business-access";
 
-// Cadeia preservada pela V131: SecureBetaAppV100 → SecureBetaAppV97 →
-// SecureBetaAppV66 → SecureBetaAppV65. O administrador mantém o sistema
-// completo, o encarregado recebe o painel limitado e o colaborador acessa
-// somente o próprio ponto.
+// O administrador acessa o sistema completo pelo Google autorizado.
+// A senha master do encarregado libera somente o portal /ponto.
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const requestHeaders = await headers();
   const email = authenticatedEmailFromHeaders(requestHeaders);
-  const isAdmin = email === SOLE_ADMIN_EMAIL;
 
-  if (isAdmin) {
+  if (email === SOLE_ADMIN_EMAIL) {
     return (
       <SecureBetaAppV131
         userName="Samuel Scolari"
@@ -33,31 +27,8 @@ export default async function Home() {
     );
   }
 
-  if (!hasStaffSessionCookie(requestHeaders)) {
-    return <AccessGate nextPath="/" />;
-  }
+  const masterSession = await masterPointSessionFromHeaders(requestHeaders);
+  if (masterSession) redirect("/ponto");
 
-  const staff = await businessStaffSessionFromHeaders(requestHeaders);
-  if (!staff) {
-    return (
-      <AccessGate
-        nextPath="/"
-        message="Sua sessão expirou. Entre novamente com matrícula e senha."
-      />
-    );
-  }
-
-  if (staff.role === "colaborador") {
-    redirect("/ponto");
-  }
-
-  return (
-    <SecureBetaAppV131
-      userName={staff.name}
-      userEmail={null}
-      isAdmin={false}
-      accessRole="encarregado"
-      employeeCode={staff.registration}
-    />
-  );
+  return <AccessGate nextPath="/" />;
 }
